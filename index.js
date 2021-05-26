181f7353a8afe7e1409f8b197e6910c5b0e902e0
Sujinathakuri <sujchand2@gmail.com>
2021-05-25 20:47:15 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-const returnFirstTwoDrivers = (drivers) => { return drivers.slice(0, 2) }
-
-const returnLastTwoDrivers = (drivers) => { return drivers.slice(2) }
-
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
-
-function createFareMultiplier(integer) {
-    return function fareMultiplier(fare) {
-        return fare * integer
-    }
+const divide = function() {
+    return 2000 / 100
 }
 
-const fareDoubler = (createFareMultiplier(2))
+const square = (parameter1) => parameter1 * parameter1;
 
-const fareTripler = (createFareMultiplier(3))
-
-function selectDifferentDrivers(drivers, oneOfTheFunctions) {
-    return oneOfTheFunctions(drivers);
-}
+const add = (param1, param2) => param1 + param2;
